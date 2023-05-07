c2e6a9baca4cada79699174f884c405e231b17cc
박민우 <alsd20177@gmail.com>
2023-05-07 22:47:48 +0900

    [FEATURE] #2 - Header 컴포넌트 구현
    메인 페이지의 제목 구현

--- fridge-web/src/Components/MainPage/Header/Header.jsx
+++ fridge-web/src/Components/MainPage/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import styles from './Header.module.css'
 
 export default function Header() {
     return (
-        <div>
-            
-        </div>
+        <header className={styles.header}>
+                <h1 className={styles.title}>냉장고 식재료 현황</h1> 
+        </header>
     );
 }
-
