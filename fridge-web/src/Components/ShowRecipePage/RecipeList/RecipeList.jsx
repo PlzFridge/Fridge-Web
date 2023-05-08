f6b3bda69c166dc64badb0823d37f55a8f237cf4
박민우 <alsd20177@gmail.com>
2023-05-08 18:17:16 +0900

    [FEATURE] #6 - RecipeList 컴포넌트 구현 및 더미 데이터 생성

--- fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
+++ fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import styles from "./RecipeList.module.css";
 
 export default function RecipeList() {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  // dummy data
+  const [recipeList, setRecipeList] = useState([
+    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://qwafsdfas", 
+        exist_list: ["김치", "감자"], not_exist_list: ["마늘", "양파"], carbon_output: 860},
+    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://qwafsdfas", 
+        exist_list: ["김치", "감자"], not_exist_list: ["마늘", "양파"], carbon_output: 860},
+    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://qwafsdfas", 
+        exist_list: ["김치", "감자"], not_exist_list: ["마늘", "양파"], carbon_output: 860}
+  ]);
 
+  return (
+    <ul className={styles.container}>
+        {recipeList.map((item) => (
+                <li key={item.recipe_id} className={styles.recipe}>
+                    <p className={styles.recipe__name}>{item.recipe_name}</p>
+                    <p className={styles.carbon__output}>{item.carbon_output}</p>
+                    <img src={item.recipe_img} alt="recipe_img"/>
+                    <p className={styles.exist__list}>{item.exist_list}</p>
+                    <p className={styles.not__exist__list}>{item.not_exist_list}</p>
+                </li>   
+        ))}
+    </ul>
+  );
+}
