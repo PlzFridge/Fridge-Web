05ca4c1c2453b2d83384d858ab5024ead7a78f02
박민우 <alsd20177@gmail.com>
2023-05-08 20:50:43 +0900

    [CSS] #6 - 레시피 추천 화면 CSS 설정
    각 컴포넌트 내부 아이템 설정
    레시피 컴포넌트 그림자 설정, hover 시 배경 변경 설정

--- fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
+++ fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
@@ -5,11 +5,11 @@ import styles from "./RecipeList.module.css";
 export default function RecipeList() {
   // dummy data
   const [recipeList, setRecipeList] = useState([
-    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://qwafsdfas", 
+    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://user-images.githubusercontent.com/31370590/236804229-35e565ad-601e-4c3b-8215-63b0c173413b.png", 
         exist_list: ["김치", "감자"], not_exist_list: ["마늘", "양파"], carbon_output: 860},
-    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://qwafsdfas", 
+    { recipe_id: 1, recipe_name: "김치볶음밥", recipe_img: "https://user-images.githubusercontent.com/31370590/236810148-ff27e0ec-d8c9-4752-96ff-e48b4d9a6302.png", 
         exist_list: ["김치", "감자"], not_exist_list: ["마늘", "양파"], carbon_output: 860},
-    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://qwafsdfas", 
+    { recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://user-images.githubusercontent.com/31370590/236804229-35e565ad-601e-4c3b-8215-63b0c173413b.png", 
         exist_list: ["김치", "감자"], not_exist_list: ["마늘", "양파"], carbon_output: 860}
   ]);
 
@@ -17,11 +17,17 @@ export default function RecipeList() {
     <ul className={styles.container}>
         {recipeList.map((item) => (
                 <li key={item.recipe_id} className={styles.recipe}>
-                    <p className={styles.recipe__name}>{item.recipe_name}</p>
-                    <p className={styles.carbon__output}>{item.carbon_output}</p>
-                    <img src={item.recipe_img} alt="recipe_img"/>
-                    <p className={styles.exist__list}>{item.exist_list}</p>
-                    <p className={styles.not__exist__list}>{item.not_exist_list}</p>
+                    <div className={styles.recipe__header}>
+                      <span className={styles.recipe__name}>{item.recipe_name}</span>
+                      <span className={styles.carbon__output}>{item.carbon_output + "g"}</span>
+                    </div>
+                    <div className={styles.recipe__content}>
+                      <img className={styles.recipe__img} src={item.recipe_img} alt="recipe_img"/>
+                      <div className={styles.ingredient__list}>
+                        <span className={styles.exist__list}>{"냉장고에 있는 재료 : " + item.exist_list.join(" ")}</span>
+                        <span className={styles.not__exist__list}>{"사야하는 재료 : " + item.not_exist_list.join(" ")}</span>
+                      </div>
+                    </div>
                 </li>   
         ))}
     </ul>
