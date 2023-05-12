3370c4dc4fcc43189a67e86046d0e4b413361692
박민우 <alsd20177@gmail.com>
2023-05-13 02:31:37 +0900

    [CSS] #14 - Navbar 디자인
    아이콘 추가 및 폰트, 색상 설정

--- fridge-web/src/Components/Navbar/Navbar.jsx
+++ fridge-web/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+import {BiBowlRice} from 'react-icons/bi'
+import {TbPlusMinus} from 'react-icons/tb'
+import {FaBook} from 'react-icons/fa'   
+import {GiHamburgerMenu} from 'react-icons/gi'   
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link to='/'>Fridge</Link>
-            <Link to='/addDelete'>Add/Delete</Link>
-            <Link to='/showRecipe'>Recipe</Link>
+        <nav className={styles.navbar}>
+            <div className={styles.menu}>
+                <div className={styles.menu__title__bar}>Menu Bar</div> 
+            </div>
+            <Link to='/' className={styles.menu}>
+                <BiBowlRice className={styles.icon}/>
+                <div className={styles.menu__title}>Fridge</div>
+            </Link>
+            <Link to='/addDelete' className={styles.menu}>
+                <TbPlusMinus className={styles.icon}/>
+                <div className={styles.menu__title}>Add/Delete</div>
+            </Link>
+            <Link to='/showRecipe' className={styles.menu}>
+                <FaBook className={styles.icon}/>
+                <div className={styles.menu__title}>Recipe</div>
+            </Link>
         </nav>
     );
 }
--- fridge-web/src/Components/Navbar/Root.jsx
+++ fridge-web/src/Components/Navbar/Root.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
-import styles from "./ShowRecipePage.module.css";
+import styles from "./Root.module.css"
 
 export default function Root() {
   return (
-    <div>
-      <Navbar />
-      <Outlet className={styles.outlet} />
-    </div>
+    <>
+      <Navbar/>
+      <Outlet/>
+    </>
   );
 }
