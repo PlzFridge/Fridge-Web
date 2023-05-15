2784b1e73228f4f26158fcdf41c01e2ec0d3c396
박민우 <alsd20177@gmail.com>
2023-05-15 15:01:40 +0900

    [FEATURE] #14 - 메인 페이지 화면 이동 버튼 로직 구현

--- fridge-web/src/Components/MainPage/Footer/Footer.jsx
+++ fridge-web/src/Components/MainPage/Footer/Footer.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
+import { useNavigate } from "react-router-dom";
 
-export default function Footer() {
+export default function Footer() {  
+
+  const navigate = useNavigate();
+  
+  const moveToAddDelete = () => {
+    navigate(`/addDelete`);
+  };
+
+  const moveToShowRecipe = () => {
+    navigate(`/showRecipe`);
+  };
+
   return (
     <div className={styles.footer}>
       <div className={styles.btn__container}>
-        <button className={styles.btn}>식재료 추가/삭제</button>
-        <button className={styles.btn}>레시피 추천 받기</button>
+        <button className={styles.btn} onClick={moveToAddDelete}>
+          식재료 추가/삭제
+        </button>
+        <button className={styles.btn} onClick={moveToShowRecipe}>
+          레시피 추천 받기
+        </button>
       </div>
     </div>
   );
