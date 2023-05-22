b26fb7b3b068950a4b8e6ee0e252d484adca6296
박민우 <alsd20177@gmail.com>
2023-05-22 13:46:33 +0900

    [FEATURE] #14 - 레시피 추천 API 연결

--- fridge-web/src/Components/RecipeDetailPage/RecipeDetailPage.jsx
+++ fridge-web/src/Components/RecipeDetailPage/RecipeDetailPage.jsx
@@ -1,91 +1,117 @@
-import React from 'react';
-import styles from './RecipeDetailPage.module.css'
-import { useState, useRef } from 'react';
-import RecipeModal from './RecipeModal/RecipeModal';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import styles from "./RecipeDetailPage.module.css";
+import { useState, useRef } from "react";
+import RecipeModal from "./RecipeModal/RecipeModal";
+import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export default function RecipeDetailPage() {
-
   //dummy data
-  const [recipeDetail, setRecipeDetail] = useState(
-    {recipe_id: 1, recipe_name: "김치찌개", recipe_img: "https://user-images.githubusercontent.com/31370590/236804229-35e565ad-601e-4c3b-8215-63b0c173413b.png", 
-    exist_list: ["김치", "감자"], not_exist_list: ["마늘", "양파"], process: "https://www.youtube.com", carbon_output: 860}
-  );
-
+  const [recipeDetail, setRecipeDetail] = useState({
+    recipe_id: 1,
+    recipe_name: "김치찌개",
+    recipe_img:
+      "https://user-images.githubusercontent.com/31370590/236804229-35e565ad-601e-4c3b-8215-63b0c173413b.png",
+    exist_list: ["김치", "감자"],
+    not_exist_list: ["마늘", "양파"],
+    process: "https://www.youtube.com",
+    carbon_output: 860,
+  });
 
   // Navigate 관련
-  const { recipeId } = useParams(); 
+  const { recipeId } = useParams();
   console.log(recipeId);
 
   const navigate = useNavigate();
 
-
   // 모달창 관련
-  const [modalOpen, setModalOpen] = useState(false); // 모달창의 오픈 여부 
+  const [modalOpen, setModalOpen] = useState(false); // 모달창의 오픈 여부
   const myRecipe = useRef(null);
 
   const openModal = () => {
-    setModalOpen((prev)=>(!prev));
-  }
+    setModalOpen((prev) => !prev);
+  };
 
   const closeModal = () => {
-    setModalOpen((prev)=>(!prev));
-    // 모달이 닫혔다는 것은 
+    setModalOpen((prev) => !prev);
+    // 모달이 닫혔다는 것은
     // 레시피를 선택타고 사용한 재료도 서버에 전달했으므로
     // 이제 메인 화면으로 가는 로직 추가
     navigate(`/`);
-  }
-  
+  };
+
   return (
     <div className={styles.container}>
-      { !modalOpen ? (
-          <div ref={myRecipe} className={styles.show__recipe}>
-        <header className={styles.header}>
-          <h1 className={styles.title}> {recipeDetail.recipe_name} </h1>
-        </header>
+      {!modalOpen ? (
+        <div ref={myRecipe} className={styles.show__recipe}>
+          <header className={styles.header}>
+            <h1 className={styles.title}> {recipeDetail.recipe_name} </h1>
+          </header>
 
-        <img className={styles.recipe__img} src={recipeDetail.recipe_img} alt="recipe_img"/>
+          <img
+            className={styles.recipe__img}
+            src={recipeDetail.recipe_img}
+            alt="recipe_img"
+          />
 
-        <div className={styles.ingredient__list}>
-          필요한 재료 :&nbsp;
-          <span className={styles.not__exist__list}>
-            {recipeDetail.not_exist_list.join(" ")}
-          </span> 
-          &nbsp;
-          <span className={styles.exist__list}>
-            {recipeDetail.exist_list.join(" ")}
-          </span>
-        </div>
-
-        <a className={styles.process} href={recipeDetail.process} target='blank'>조리 과정 보기</a>
-
-        <div className={styles.carbon__output__container}>
-          <span className={styles.carbon__text}>
-            {"이 음식을 만드는 데 필요한 "} <span className={styles.exist__list}>{recipeDetail.exist_list.join(", ")}</span>{"를"}
-          </span>
-          <span className={styles.carbon__text}>
-            {"생산하는데 발생되는 탄소량은 "} <span className={styles.carbon__output}>{recipeDetail.carbon_output}{"g"}</span>
-            {" 입니다."}
-          </span>
-          <div className={styles.carbon__text__container}>
-            <span className={styles.carbon__text}>
-              {"이는 승용차 1대가 32.7km 이동 시 배출하는 탄소량이며"}
+          <div className={styles.ingredient__list}>
+            필요한 재료 :&nbsp;
+            <span className={styles.not__exist__list}>
+              {recipeDetail.not_exist_list.join(" ")}
             </span>
-            <span className={styles.carbon__text}>
-              {"소나무 1.2그루가 1년간 흡수하는 이산화탄소량과 같습니다"}
+            &nbsp;
+            <span className={styles.exist__list}>
+              {recipeDetail.exist_list.join(" ")}
             </span>
           </div>
-        </div>
 
-        <div className={styles.btn__container}>
-          <button className={styles.later__btn}>나중에 먹을래요</button>
-          <button className={styles.ate__btn} onClick={openModal}>만들어 먹었어요</button>
+          <a
+            className={styles.process}
+            href={recipeDetail.process}
+            target="blank"
+          >
+            조리 과정 보기
+          </a>
+
+          <div className={styles.carbon__output__container}>
+            <span className={styles.carbon__text}>
+              {"이 음식을 만드는 데 필요한 "}{" "}
+              <span className={styles.exist__list}>
+                {recipeDetail.exist_list.join(", ")}
+              </span>
+              {"를"}
+            </span>
+            <span className={styles.carbon__text}>
+              {"생산하는데 발생되는 탄소량은 "}{" "}
+              <span className={styles.carbon__output}>
+                {recipeDetail.carbon_output}
+                {"g"}
+              </span>
+              {" 입니다."}
+            </span>
+            <div className={styles.carbon__text__container}>
+              <span className={styles.carbon__text}>
+                {"이는 승용차 1대가 32.7km 이동 시 배출하는 탄소량이며"}
+              </span>
+              <span className={styles.carbon__text}>
+                {"소나무 1.2그루가 1년간 흡수하는 이산화탄소량과 같습니다"}
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.btn__container}>
+            <button className={styles.later__btn}>나중에 먹을래요</button>
+            <button className={styles.ate__btn} onClick={openModal}>
+              만들어 먹었어요
+            </button>
+          </div>
         </div>
-      </div>
-        ) : (
-          <RecipeModal existList={recipeDetail.exist_list} closeModal={closeModal}/>
-        )}
+      ) : (
+        <RecipeModal
+          existList={recipeDetail.exist_list}
+          closeModal={closeModal}
+        />
+      )}
     </div>
   );
 }
--- fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
+++ fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
@@ -2,39 +2,55 @@ import React from "react";
 import { useState, useEffect } from "react";
 import styles from "./RecipeList.module.css";
 import { useNavigate } from "react-router-dom";
+import ingredientList from "./../../API/ingredientList";  
+import instance from "../../API/api";
 
 export default function RecipeList() {
-
   // dummy data
   const [recipeList, setRecipeList] = useState([
     {
-      recipe_id: 1,
-      recipe_name: "김치찌개",
-      recipe_img:
+      recipeId: 1,
+      recipeName: "김치찌개",
+      recipeImg:
         "https://user-images.githubusercontent.com/31370590/236804229-35e565ad-601e-4c3b-8215-63b0c173413b.png",
-      exist_list: ["김치", "감자"],
-      not_exist_list: ["마늘", "양파"],
-      carbon_output: 860,
+      ingredientList: ["", ""],
+      method: "",
+      existList: ["김치", "감자"],
+      notExistList: ["마늘", "양파"],
+      carbon: 860,
     },
     {
-      recipe_id: 2,
-      recipe_name: "김치볶음밥",
-      recipe_img:
+      recipeId: 2,
+      recipeName: "김치볶음밥",
+      recipeImg:
         "https://user-images.githubusercontent.com/31370590/236810148-ff27e0ec-d8c9-4752-96ff-e48b4d9a6302.png",
-      exist_list: ["김치", "감자"],
-      not_exist_list: ["마늘", "양파"],
-      carbon_output: 860,
+      existList: ["김치", "감자"],
+      notExistList: ["마늘", "양파"],
+      carbon: 860,
     },
     {
-      recipe_id: 3,
-      recipe_name: "김치찌개",
-      recipe_img:
+      recipeId: 3,
+      recipeName: "김치찌개",
+      recipeImg:
         "https://user-images.githubusercontent.com/31370590/236804229-35e565ad-601e-4c3b-8215-63b0c173413b.png",
-      exist_list: ["김치", "감자"],
-      not_exist_list: ["마늘", "양파"],
-      carbon_output: 860,
+      existList: ["김치", "감자"],
+      notExistList: ["마늘", "양파"],
+      carbon: 860,
     },
   ]);
+
+  const getRecipeListAPI = async () => {
+    try {
+      const response = await instance.get("/recommend");  
+      setRecipeList(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getRecipeListAPI();
+  }, []);
 
   const navigate = useNavigate();
 
@@ -46,28 +62,26 @@ export default function RecipeList() {
     <ul className={styles.container}>
       {recipeList.map((item) => (
         <li
-          key={item.recipe_id}
+          key={item.recipeId}
           className={styles.recipe}
-          onClick={() => showRecipeDetail(item.recipe_id)}
+          onClick={() => showRecipeDetail(item.recipeId)}
         >
           <div className={styles.recipe__header}>
-            <span className={styles.recipe__name}>{item.recipe_name}</span>
-            <span className={styles.carbon__output}>
-              {item.carbon_output + "g"}
-            </span>
+            <span className={styles.recipe__name}>{item.recipeName}</span>
+            <span className={styles.carbon__output}>{item.carbon + "g"}</span>
           </div>
           <div className={styles.recipe__content}>
             <img
               className={styles.recipe__img}
-              src={item.recipe_img}
+              src={item.recipeImg}
               alt="recipe_img"
             />
             <div className={styles.ingredient__list}>
               <span className={styles.exist__list}>
-                {"냉장고에 있는 재료 : " + item.exist_list.join(" ")}
+                {"냉장고에 있는 재료 : " + item.existList.join(" ")}
               </span>
               <span className={styles.not__exist__list}>
-                {"사야하는 재료 : " + item.not_exist_list.join(" ")}
+                {"사야하는 재료 : " + item.notExistList.join(" ")}
               </span>
             </div>
           </div>
