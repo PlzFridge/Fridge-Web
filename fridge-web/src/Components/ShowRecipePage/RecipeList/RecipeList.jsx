a75233f8d7a1da97e04cf72a544455dccac5f966
박민우 <alsd20177@gmail.com>
2023-06-17 00:49:42 +0900

    [FIX] #18 - 최종 수정

--- fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
+++ fridge-web/src/Components/ShowRecipePage/RecipeList/RecipeList.jsx
@@ -7,37 +7,7 @@ import instance from "../../API/api";
 
 export default function RecipeList() {
   // dummy data
-  const [recipeList, setRecipeList] = useState([
-    {
-      recipeId: 1,
-      recipeName: "김치찌개",
-      recipeImg:
-        "https://github.com/PlzFridge/Fridge-Web/assets/31370590/bc8a33d8-dded-486e-bcce-4cf71d7aae3e",
-      ingredientList: ["", ""],
-      method: "",
-      existList: ["김치", "감자"],
-      notExistList: ["마늘", "양파"],
-      carbon: 500,
-    },
-    {
-      recipeId: 2,
-      recipeName: "김치볶음밥",
-      recipeImg:
-        "https://github.com/PlzFridge/Fridge-Web/assets/31370590/f88f67fe-0b7b-482f-9992-2b99eeca6d11",
-      existList: ["김치", "감자"],
-      notExistList: ["마늘", "양파"],
-      carbon: 860,
-    },
-    {
-      recipeId: 3,
-      recipeName: "김치찌개",
-      recipeImg:
-        "https://user-images.githubusercontent.com/31370590/236804229-35e565ad-601e-4c3b-8215-63b0c173413b.png",
-      existList: ["김치", "감자"],
-      notExistList: [],
-      carbon: 860,
-    },
-  ]);
+  const [recipeList, setRecipeList] = useState([]);
 
   const getRecipeListAPI = async () => {
     try {
